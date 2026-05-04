120347d8a091a683f0d8a9b9d3ab79c61f74de6c
RajkiranDev123 <rajtech645@gmail.com>
2026-05-04 11:18:43 +0530

    cookies

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -29,11 +29,17 @@ export const signup = async (req, res) => {
       email,
       password,
     });
+    // res.cookie(name, value, options);
+    // small data stored in browser that is automatically sent to the server with every request
     res.cookie("jwt", createToken(email, user._id), {
+      httpOnly: true, // JS (frontend) cannot access cookie
       maxAge,
       secure: true, // cookie will only be sent over HTTPS.
-      sameSite: "None", // cookie can be sent in cross-site requests
-      // Browsers require secure: true when sameSite is "None". Otherwise, the cookie will be rejected.
+      sameSite: "none", // cookie can be sent in cross-site requests
+      // "strict" → safest , "lax" → balanced (commonly used) , "none" → requires secure: true
+      // “Allow cross-site cookie, but only over encrypted connection (https)”
+      //  secure: false, sameSite: "lax" ==> dev
+      //  prod ==>   secure: true , sameSite: "none",
     });
     return res.status(201).json({
       user: {
