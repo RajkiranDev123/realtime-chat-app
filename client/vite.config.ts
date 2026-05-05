99bf5dc87db72c7f74d3c51dfe4434bbca6e09ff
RajkiranDev123 <rajtech645@gmail.com>
2026-05-05 09:16:04 +0530

    configs

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,7 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,4 +11,19 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
+
+
+
+// Build = preparing your app for real users (production)
+// Build = converting your project into optimized, deployable files for users
+
+// Vite is not just a bundler — it’s a fast dev server + bundler (only during build).
+// A bundler is a tool that combines many files into a few optimized files.
+
+// It’s the configuration (settings) file for Vite
+
+// In simple words :  “This file tells Vite how to run and build your project.”
+
+// In dev mode, vite.config.js configures how Vite serves and processes your code — not how it bundles it.
+
