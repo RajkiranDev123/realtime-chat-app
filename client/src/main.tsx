49167ec796822beaa6268a18624b60fea7cdf48f
RajkiranDev123 <rajtech645@gmail.com>
2026-05-05 10:33:47 +0530

    zustand

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+// It takes your React app and attaches it to a real HTML element
+// <div id="root"></div>
 import "./index.css";
 import App from "./App.tsx";
 import { Toaster } from "./components/ui/sonner.tsx";
