663111f2b68ec26e1eacaa79edc319b7d74a85ad
RajkiranDev123 <rajtech645@gmail.com>
2026-05-07 11:09:50 +0530

    multer

--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -5,16 +5,28 @@ import {
   login,
   getUserInfo,
   updateProfile,
-  addProfileImage
+  addProfileImage,
+  removeProfileImage,
 } from "../controllers/AuthController.js";
 import { verifyToken } from "../middlewares/AuthMiddleware.js";
+import multer from "multer";
 
 const authRoutes = Router();
+
+const upload = multer({ dest: "uploads/profiles/" });
 
 authRoutes.post("/signup", signup);
 authRoutes.post("/login", login);
 authRoutes.get("/user-info", verifyToken, getUserInfo);
 authRoutes.post("/update-profile", verifyToken, updateProfile);
-authRoutes.post("/add-profile-image", verifyToken, addProfileImage);
+authRoutes.post(
+  "/add-profile-image",
+  verifyToken,
+  upload.single("profile-image"),
+  // "profile-image" → must match the frontend input name
+  // <input type="file" name="profile-image" />
+  addProfileImage,
+);
+authRoutes.delete("/remove-profile-image", verifyToken, removeProfileImage);
 
 export default authRoutes;
