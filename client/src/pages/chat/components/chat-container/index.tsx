79a58dce0886470fea49610b3d756a285fd51140
RajkiranDev123 <rajtech645@gmail.com>
2026-05-12 15:54:22 +0530

    chat header

--- client/src/pages/chat/components/chat-container/index.tsx
+++ client/src/pages/chat/components/chat-container/index.tsx
@@ -5,9 +5,11 @@ import MessageContainer from "./components/message-container";
 const ChatContainer = () => {
   return (
     // h-screen == h-[100vh]
+    // A div with position: fixed and no width often shrinks to the width of its content.
+    // and its children can only grow within that width.
     <div
-      className="fixed top-0 text-white  h-[100vh] bg-[#1c1d25] 
-    flex flex-col md:flex-1 md:static"
+      className="fixed top-0  text-white  h-[100vh] bg-[#1c1d25] 
+    flex flex-col md:flex-1 md:static w-full"
     >
       {/* static = default normal positioning. So it overrides : fixed */}
       {/* On medium screens and larger , position becomes static  */}
