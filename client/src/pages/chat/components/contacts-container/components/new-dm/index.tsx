de4c23050e285636704add74b217d50ccc35547f
RajkiranDev123 <rajtech645@gmail.com>
2026-05-15 12:29:15 +0530

    bug fix

--- client/src/pages/chat/components/contacts-container/components/new-dm/index.tsx
+++ client/src/pages/chat/components/contacts-container/components/new-dm/index.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { useState } from "react";
 
-const index = () => {
+import { FaPlus } from "react-icons/fa";
+
+const NewDm = () => {
+  const [openNewContactModal, setOpenNewContactModal] = useState(false);
+
+  const searchContacts = async () => {};
   return (
-    <div>index</div>
-  )
-}
+    <>
+      {/* tooltip */}
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <FaPlus
+              onClick={() => setOpenNewContactModal(true)}
+              className="text-neutral-400/90 text-xl font-light text-start hover:text-neutral-100
+          cursor-pointer transition-all duration-300"
+            />
+          </TooltipTrigger>
+          <TooltipContent className="bg-black border-none text-white p-2">
+            Select New Contact
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+      {/* tooltip */}
 
-export default index
+      {/* dialog */}
+
+      
+
+      {/* dialog */}
+    </>
+  );
+};
+
+export default NewDm;
