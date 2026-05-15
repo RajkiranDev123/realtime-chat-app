1fae98816803b7e8a322ac31518480b19609949d
RajkiranDev123 <rajtech645@gmail.com>
2026-05-15 13:38:30 +0530

    contact controller

--- client/src/pages/chat/components/contacts-container/components/new-dm/index.tsx
+++ client/src/pages/chat/components/contacts-container/components/new-dm/index.tsx
@@ -1,17 +1,28 @@
 import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { animationDefaultOptions } from "@/lib/utils";
 import { useState } from "react";
 
 import { FaPlus } from "react-icons/fa";
+import Lottie from "react-lottie";
 
 const NewDm = () => {
   const [openNewContactModal, setOpenNewContactModal] = useState(false);
+  const [searchedContacts, setSearchedContacts] = useState([]);
 
-  const searchContacts = async () => {};
+  const searchContacts = async (searchTerm: string) => {};
   return (
     <>
       {/* tooltip */}
@@ -32,8 +43,44 @@ const NewDm = () => {
       {/* tooltip */}
 
       {/* dialog */}
-
-      
+      <Dialog open={openNewContactModal} onOpenChange={setOpenNewContactModal}>
+        <DialogContent className="bg-[#181920] border-none text-white w-[400px] h-[400px] flex flex-col">
+          <DialogHeader>
+            <DialogTitle>Please select a contact</DialogTitle>
+            <DialogDescription></DialogDescription>
+          </DialogHeader>
+          <div>
+            <Input
+              placeholder="Search Contacts"
+              className="rounded-lg p-8 bg-[#2c2e3b] border-none"
+              onChange={(e) => searchContacts(e.target.value)}
+            />
+          </div>
+          {searchedContacts.length <= 0 && (
+            <div
+              className="flex-1 md:bg-[#1c1d25] md:flex flex-col justify-center 
+            items-center  duration-1000 transition-all"
+            >
+              <Lottie
+                isClickToPauseDisabled={true}
+                height={100}
+                width={100}
+                options={animationDefaultOptions}
+              />
+              <div
+                className=" text-white/80   mt-5
+                            lg:text-2xl text-xl  text-center "
+              >
+                <h3 className="poppins-thin-italic">
+                  Hi <span className="text-purple-500">!</span> Search
+                  <span className="text-purple-500"> new</span> contact
+                  <span className="text-purple-500">.</span>
+                </h3>
+              </div>
+            </div>
+          )}
+        </DialogContent>
+      </Dialog>
 
       {/* dialog */}
     </>
