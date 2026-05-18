6c4b674c23ea2477f9f6e322fcfe379e4f52d539
RajkiranDev123 <rajtech645@gmail.com>
2026-05-18 16:07:24 +0530

    socket setup done

--- server/models/MessageModel.js
+++ server/models/MessageModel.js
@@ -1,1 +1,47 @@
+import mongoose from "mongoose";
 
+const messageSchema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Users",
+      required: true,
+    },
+    recipient: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Users",
+      required: false,
+    },
+    messageType: {
+      type: String,
+      enum: ["text", "file"], // enum = whitelist of allowed values
+      required: true,
+    },
+    content: {
+      type: String,
+      required: function () {
+        return this.messageType === "text"; // If messageType = "text" → content must exist
+      },
+    },
+    fileUrl: {
+      type: String,
+      required: function () {
+        return this.messageType === "file";
+        // this = the current document you are saving
+        //   Message.create({
+        //   messageType: "text",
+        //   content: "Hello"
+        // })
+
+        // 1. mongoose creates an object in memory
+        // 2. Validation happens ==> validation runs on the document instance and this points to that document.
+        // 3. saved to db
+      },
+    },
+  },
+  { timestamps: true },
+);
+
+const Message = mongoose.model("Messages", messageSchema);
+
+export default Message;
