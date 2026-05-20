d540f244bacfa8610bf8266ac38a0a9426559d44
RajkiranDev123 <rajtech645@gmail.com>
2026-05-20 16:17:29 +0530

    message container feature

--- client/src/pages/chat/components/chat-container/components/message-container/index.tsx
+++ client/src/pages/chat/components/chat-container/components/message-container/index.tsx
@@ -58,17 +58,23 @@ const MessageContainer = () => {
   const renderDmMessages = (message: Message) => {
     return (
       <div
-        className={`${message.sender === selectedChatData?._id ? "text-right" : "text-left"}`}
+        className={`${message.sender === selectedChatData?._id ? "text-left" : "text-right"}`}
       >
-        <div
-          className={`${
-            message.sender !== selectedChatData?._id
-              ? "bg-[#8417ff] text-[#8417ff]/90 border-[#8417ff]/50"
-              : "bg-[#2a2b33] text-white/80 border-[#ffffff]/20"
-          } 
+        {message.messageType === "text" && (
+          <div
+            className={`${
+              message.sender !== selectedChatData?._id
+                ? "bg-[#8417ff] text-white/80 border-[#8417ff]/50"
+                : "bg-[#2a2b33] text-white/80 border-[#ffffff]/20"
+            } 
             border inline-block p-4 rounded my-1 max-w-[50%] break-words`}
-        >
-          {message.content}
+          >
+            {message.content}
+          </div>
+        )}
+
+        <div className="text-xs text-gray-600">
+          {moment(message.createdAt).format("LT")}
         </div>
       </div>
     );
@@ -84,7 +90,8 @@ const MessageContainer = () => {
 
     "
     >
-      MessageContainer
+      {renderMessages()}
+      <div ref={scrollRef} />
     </div>
   );
 };
