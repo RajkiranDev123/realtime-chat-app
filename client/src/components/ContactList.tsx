6d1b3af8ef3711ffc5725cc9ac309fde935a1e83
RajkiranDev123 <rajtech645@gmail.com>
2026-05-21 12:27:49 +0530

    contacts avatar

--- client/src/components/ContactList.tsx
+++ client/src/components/ContactList.tsx
@@ -1,4 +1,7 @@
 import { useAppStore } from "@/store";
+import { Avatar, AvatarImage } from "./ui/avatar";
+import { getColor } from "@/lib/utils";
+import { HOST } from "@/utils/constants";
 
 type Contact = {
   _id: string;
@@ -17,7 +20,7 @@ const ContactList = ({
   isChannel = false,
 }: {
   contacts: Contact[];
-  isChannel?: false;
+  isChannel?: boolean;
 }) => {
   const {
     selectedChatData,
@@ -35,7 +38,52 @@ const ContactList = ({
       setSelectedChatMessages([]);
     }
   };
-  return <div>ContactList</div>;
+  return (
+    <div className="mt-5">
+      {contacts.map((contact) => (
+        <div
+          onClick={() => handleClick(contact)}
+          key={contact._id}
+          className={`
+                pl-10 py-2 transition-all duration-300 cursor-pointer
+                ${
+                  selectedChatData && selectedChatData?._id === contact._id
+                    ? "bg-[#8417ff] hover:bg-[#8417ff]"
+                    : "hover:bg-[#f1f1f111]"
+                }
+                `}
+        >
+          <div className="flex gap-5 items-center justify-start text-neutral-300">
+            {!isChannel && (
+              <Avatar className="h-12 w-12 rounded-full overflow-hidden">
+                {selectedChatData?.image ? (
+                  <AvatarImage
+                    src={`${HOST}/${selectedChatData.image}`}
+                    alt="profile"
+                    className="object-cover w-full h-full"
+                  />
+                ) : (
+                  <div
+                    className={`uppercase h-12 w-12 text-lg border-[1px] 
+                            flex items-center justify-center
+                            rounded-full ${getColor(selectedChatData?.color ?? 0)}
+                          `}
+                    // ts says : “I can’t pass undefined into something that requires a number”
+                    // ?? ==> It gives a default value only when the left side is null or undefined.
+                    // nullish coalescing operator.
+                  >
+                    {selectedChatData?.firstName
+                      ? selectedChatData.firstName.split("").shift()
+                      : selectedChatData?.email.split("").shift()}
+                  </div>
+                )}
+              </Avatar>
+            )}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ContactList;
