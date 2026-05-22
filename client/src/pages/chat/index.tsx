2db91f126192b47c1f9cedc41e8b3c81b72094b5
RajkiranDev123 <rajtech645@gmail.com>
2026-05-22 13:22:05 +0530

    modal show image

--- client/src/pages/chat/index.tsx
+++ client/src/pages/chat/index.tsx
@@ -7,7 +7,14 @@ import EmptyChatContainer from "./components/empty-chat-container";
 import ChatContainer from "./components/chat-container";
 
 const Chat = () => {
-  const { userInfo , selectedChatType} = useAppStore();
+  const {
+    userInfo,
+    selectedChatType,
+    isUploading,
+    isDownloading,
+    fileUploadProgress,
+    fileDownloadProgress,
+  } = useAppStore();
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -21,11 +28,23 @@ const Chat = () => {
   }, [userInfo, navigate]);
   return (
     <div className="flex h-[100vh] text-white overflow-hidden gap-1 bg-gray-300 p-2">
+      {isUploading && (
+        <div
+          className="
+          h-[100vh] w-[100vw] fixed top-0 z-10 left-0 bg-black/80 
+          flex items-center justify-center flex-col gap-5 backdrop-blur-lg
+          "
+        >
+          <h5 className="text-5xl animate-pulse ">Uploading File</h5>
+          {fileUploadProgress}
+        </div>
+      )}
       <ContactsContainer />
-      {
-        selectedChatType ===undefined ? <EmptyChatContainer/> :<ChatContainer/>
-      }
-
+      {selectedChatType === undefined ? (
+        <EmptyChatContainer />
+      ) : (
+        <ChatContainer />
+      )}
     </div>
   );
 };
