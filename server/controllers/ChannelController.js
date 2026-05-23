daa208f403a7bde9a431b5911f1b32ce653d5692
RajkiranDev123 <rajtech645@gmail.com>
2026-05-23 17:22:20 +0530

    channel model

--- server/controllers/ChannelController.js
+++ server/controllers/ChannelController.js
@@ -1,3 +1,40 @@
 import Channel from "../models/ChannelModel.js";
 
 import User from "../models/UserModel.js";
+
+export const createChannel = async (req, res) => {
+  try {
+    const { name, members } = req.body;
+    const userId = req.userId;
+    const admin = await User.findById(userId);
+
+    if (!admin) {
+      return res.status(400).json({
+        message: "Admin user not found",
+      });
+    }
+
+    const validMembers = await User.find({ _id: { $in: members } });
+    if (validMembers.length !== members.length) {
+      return res.status(400).json({
+        success: false,
+        message: "Some members are not valid users.",
+      });
+    }
+    const newChannel = new Channel({
+      name,
+      members,
+      admin: userId,
+    });
+    await newChannel.save();
+    return res.status(201).json({
+      success: true,
+      channel: newChannel,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Internal Server error",
+    });
+  }
+};
