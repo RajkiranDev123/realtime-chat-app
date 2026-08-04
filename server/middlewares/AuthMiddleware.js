ac55d7ab40156d8c3e126058b7a92e137b7c55f9
RajkiranDev123 <rajtech645@gmail.com>
2026-08-04 15:23:10 +0530

    jwt token

--- server/middlewares/AuthMiddleware.js
+++ server/middlewares/AuthMiddleware.js
@@ -2,6 +2,8 @@ import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies?.jwt;
+
+  // or const {jwt} = req.cokkies
   // console.log("cookies ==> ", req.cookies);
 
   if (!token) {
@@ -28,7 +30,46 @@ export const verifyToken = (req, res, next) => {
     }
 
     req.userId = decoded.userId;
-    next();
+    next(); // next() runs after jwt.verify has finished and decoded is available.
   });
-  // console.log(7) // will run before decoding
+  // console.log(7) // will run before decoding finishes
 };
+
+// 1st way : Synchronous
+
+// try {
+//   const decoded = jwt.verify(token, process.env.JWT_KEY);
+
+//   req.userId = decoded.userId;
+//   next();
+// } catch (err) {
+//   return res.status(401).json({
+//     success: false,
+//     message: "Invalid token",
+//   });
+// }
+
+//////////////////////////////////////////////////////////////
+
+// 2. Promise + async/await (by wrapping it)
+
+// function verifyToken(token) {
+//   return new Promise((resolve, reject) => {
+//     jwt.verify(token, process.env.JWT_KEY, (err, decoded) => {
+//       if (err) return reject(err);
+//       resolve(decoded);
+//     });
+//   });
+// }
+
+// try {
+//   const decoded = await verifyToken(token);
+
+//   req.userId = decoded.userId;
+//   next();
+// } catch (err) {
+//   return res.status(401).json({
+//     success: false,
+//     message: "Invalid token",
+//   });
+// }
