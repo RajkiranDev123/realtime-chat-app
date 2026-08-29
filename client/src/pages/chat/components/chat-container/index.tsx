4c0158539179c282fa2f9e9601d05db5a0948cb3
RajkiranDev123 <rajtech645@gmail.com>
2026-08-29 08:06:04 +0530

    css positioning

--- client/src/pages/chat/components/chat-container/index.tsx
+++ client/src/pages/chat/components/chat-container/index.tsx
@@ -8,6 +8,10 @@ const ChatContainer = () => {
     // A div with position: fixed and no width shrinks to the width of its content.
     // and its children can only grow within that width.
     // xs sm md lg xl 2xl..............
+    // 480 640 768 1024 1280 1536
+
+    // You have two conflicting rules : fixed and md:static , after 768 its static
+    // also static  → z-index normally doesn't work and no top etc
     <div
       className="fixed top-0  flex flex-col rounded-sm text-white  h-[98vh] bg-[#1c1d25] 
                  md:flex-1 md:static w-full"
